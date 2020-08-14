ec04fe4f808d787c42effb9f6ff2f8ffd0cc991b
Hitesh Thakor <hiteshthakor.mail@gmail.com>
2020-08-14 22:28:56 +0530

    method export

--- www/FileTransferUtilPlugin.js
+++ www/FileTransferUtilPlugin.js
@@ -1,27 +1,27 @@
 var exec = require('cordova/exec');
- var HotSpotUtil = {};
+ //var HotSpotUtil = {};
 
- HotSpotUtil.enableAccessPoint = function(settings, successCallBack, failureCallBack)
-{
-    exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'enableHostAP', [settings]);
-};
-
-HotSpotUtil.disableAccessPoint = function(successCallBack, failureCallBack)
-{
-  exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'disableHostAP',[]);
-};
-
-// exports.enableAccessPoint = function(settings, successCallBack, failureCallBack)
+//  HotSpotUtil.enableAccessPoint = function(settings, successCallBack, failureCallBack)
 // {
 //     exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'enableHostAP', [settings]);
 // };
 
-// exports.disableAccessPoint = function(successCallBack, failureCallBack)
+// HotSpotUtil.disableAccessPoint = function(successCallBack, failureCallBack)
 // {
 //   exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'disableHostAP',[]);
 // };
 
-module.exports = HotSpotUtil;
+module.exports.enableAccessPoint = function(settings, successCallBack, failureCallBack)
+{
+    exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'enableHostAP', [settings]);
+};
+
+module.exports.disableAccessPoint = function(successCallBack, failureCallBack)
+{
+  exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'disableHostAP',[]);
+};
+
+//module.exports = HotSpotUtil;
 
 // exports.coolMethod = function (arg0, success, error) {
     // exec(success, error, 'FileTransferUtilPlugin', 'coolMethod', [arg0]);
