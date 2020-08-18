4b0a98fd6ff552817bc1b13bc33ef3f4ed7b4ae3
Hitesh Thakor <hiteshthakor.mail@gmail.com>
2020-08-18 16:45:40 +0530

    @0.1.6

--- www/FileTransferUtilPlugin.js
+++ www/FileTransferUtilPlugin.js
@@ -1,11 +1,11 @@
 var exec = require('cordova/exec');
 
 
-modules.exports.enableAccessPoint = function (settings, successCallBack, failureCallBack) {
+exports.enableAccessPoint = function (settings, successCallBack, failureCallBack) {
   exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'enableHostAP', [settings]);
 };
 
-modules.exports.disableAccessPoint = function (successCallBack, failureCallBack) {
+exports.disableAccessPoint = function (successCallBack, failureCallBack) {
   exec(successCallBack, failureCallBack, 'FileTransferUtilPlugin', 'disableHostAP', []);
 };
 
